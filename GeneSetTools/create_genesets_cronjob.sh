fdbde7ae0e0a4c85247de1f0c55d2dc61ea20434
risserlin <ruth.isserlin@utoronto.ca>
2016-08-31 09:10:34 -0400

    updated cronjob file with paths specific to new vm

--- GeneSetTools/create_genesets_cronjob.sh
+++ GeneSetTools/create_genesets_cronjob.sh
@@ -1,16 +1,14 @@
 
-export PATH=/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Commands:$PATH
+export PATH=/usr/lib/jvm/java-6-oracle/bin:$PATH
 
-export PATH=/Users/risserlin/bin:$PATH
-
-export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Home
+export JAVA_HOME=/usr/lib/jvm/java-6-oracle/
 
 #for required perl libraries for drugbank perl parser
 eval $(perl -I$HOME/perl5/lib/perl5 -Mlocal::lib)
 export PERL5LIB=${PERL5LIB}:~/lib/perl5:~/lib/perl5/lib64/perl5:~/lib/perl5/lib:~/lib/perl5/lib/i386-linux-thread-multi/:~/lib/perl5/lib/perl5/site_perl
 
 BUILDLOG=`date '+%B_%d_%Y'`_build.log
-ABSPATH=/Users/risserlin/EnrichmentMap_monthlyGenesetBuild/GeneSetTools
+ABSPATH=/home/baderlab/EnrichmentMap_monthlyGenesetBuild/GeneSetTools
 
 cd ${ABSPATH}
 
@@ -19,4 +17,4 @@ ${ABSPATH}/create_genesets_release.sh > ${ABSPATH}/${BUILDLOG} 2>&1
 
 LOGFILE=`cat ${ABSPATH}/${BUILDLOG}`
 
-./send_mail.py   "Geneset file build log"  "${LOGFILE}"   noreply@baderlab.org ruth.isserlin@utoronto.ca   192.168.81.32
+./send_mail.py   "Geneset file build log"  "${LOGFILE}"   auto.geneset.build@gmail.com ruth.isserlin@utoronto.ca smtp.gmail.com BuildAllHumanGenesets  
