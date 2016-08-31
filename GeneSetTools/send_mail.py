8d347fba4888de3471950510a94a3496f2815e63
risserlin <ruth.isserlin@utoronto.ca>
2016-08-30 21:59:53 -0400

    updated send mail to use a generic gmail account to send out the emails as the new vm doesn't have a smtp server installed like server1 did

--- GeneSetTools/send_mail.py
+++ GeneSetTools/send_mail.py
@@ -2,13 +2,16 @@
 import smtplib
 import sys
 
-def sendmail(subject, body, fromaddr, toaddrs, smtp_server):
-    server = smtplib.SMTP(smtp_server)
+def sendmail(subject, body, fromaddr, toaddrs, smtp_server, password):
+    #server = smtplib.SMTP(smtp_server)
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    server.login(fromaddr, password)
     header = ('Subject: %s\r\nFrom: %s\r\nTo: %s\r\n\r\n' % 
         (subject, fromaddr, ', '.join(toaddrs.split())))
     email = ''.join([header, body])
     server.sendmail(fromaddr, toaddrs.split(), email)
-    server.quit()
+    server.close()
 
 def main(args=sys.argv[1:]):
     subject = args[0]
@@ -16,7 +19,8 @@ def main(args=sys.argv[1:]):
     fromaddr = args[2]
     toaddrs = args[3] 
     smtp_server = args[4]
-    sendmail(subject, body, fromaddr, toaddrs, smtp_server)
+    password = args[5]
+    sendmail(subject, body, fromaddr, toaddrs, smtp_server,password)
 
 
 if __name__ == "__main__":
