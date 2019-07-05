d3d7cc26d9dce7a3842b064566490721cb6f031c
Geneset cronjob <geneset-cron@genesetbuild.baderlab.org>
2019-07-05 13:22:17 -0400

    removed cron from the docker as the cron job now runs on the vm, not on the docker instance.

--- Docker_instance/docker-entrypoint.sh
+++ Docker_instance/docker-entrypoint.sh
@@ -4,7 +4,4 @@
 cd $HOMEDIR/EnrichmentMap_monthlyGenesetBuild/GeneSetTools
 ant -f GeneSetTools.xml jars
 
-#create crontab file
-(crontab -l ; echo "0 0 1 * * $HOMEDIR/EnrichmentMap_monthlyGenesetBuild/GeneSetTools/create_genesets_cronjob.sh") | sort - | uniq - | crontab -
-
 exec "$@"
