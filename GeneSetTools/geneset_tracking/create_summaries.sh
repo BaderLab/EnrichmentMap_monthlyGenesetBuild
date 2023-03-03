4d4402e0e00f93a828aeba911833ea61f2d10584
Ruth Isserlin <ruth.isserlin@utoronto.ca>
2023-03-03 12:24:46 -0500

    fixed errors in summaries

--- GeneSetTools/geneset_tracking/create_summaries.sh
+++ GeneSetTools/geneset_tracking/create_summaries.sh
@@ -1,17 +1,17 @@
 
 current_date=`date '+%B_%Y'`
 
-docker run --rm -it -v "$(pwd)":/home/rstudio/projects --user rstudio bioc_summaries /usr/local/bin/R -e "rmarkdown::render('/home/rstudio/projects/Geneset_tracking.Rmd',params=list(species='Human'), output_file='/home/rstudio/projects/Geneset_tracking_human_$current_date.html')" > processing_output_Human_$current_date.txt
+docker run --rm -v "$(pwd)":/home/rstudio/projects --user rstudio bioc_summaries /usr/local/bin/R -e "rmarkdown::render('/home/rstudio/projects/Geneset_tracking.Rmd',params=list(species='Human'), output_file='/home/rstudio/projects/Geneset_tracking_human_$current_date.html')" > processing_output_Human_$current_date.txt
 
 #copy the new file over to the web page
 cp "$(pwd)"/Geneset_tracking_human_$current_date.html /home/geneset-cron/geneset_build_drive/EM_Genesets/current_release/Human/README.html
 
-docker run --rm -it -v "$(pwd)":/home/rstudio/projects --user rstudio bioc_summaries /usr/local/bin/R -e "rmarkdown::render('/home/rstudio/projects/Geneset_tracking.Rmd',params=list(species='Mouse'), output_file='/home/rstudio/projects/Geneset_tracking_mouse_$current_date.html')" > processing_output_Mouse_$current_date.txt
+docker run --rm -v "$(pwd)":/home/rstudio/projects --user rstudio bioc_summaries /usr/local/bin/R -e "rmarkdown::render('/home/rstudio/projects/Geneset_tracking.Rmd',params=list(species='Mouse'), output_file='/home/rstudio/projects/Geneset_tracking_mouse_$current_date.html')" > processing_output_Mouse_$current_date.txt
 
 #copy the new file over to the web page
 cp "$(pwd)"/Geneset_tracking_mouse_$current_date.html /home/geneset-cron/geneset_build_drive/EM_Genesets/current_release/Mouse/README.html
 
-docker run --rm -it -v "$(pwd)":/home/rstudio/projects --user rstudio bioc_summaries /usr/local/bin/R -e "rmarkdown::render('/home/rstudio/projects/Geneset_tracking.Rmd',params=list(species='Rat'), output_file='/home/rstudio/projects/Geneset_tracking_rat_$current_date.html')" > processing_output_Rat_$current_date.txt
+docker run --rm -v "$(pwd)":/home/rstudio/projects --user rstudio bioc_summaries /usr/local/bin/R -e "rmarkdown::render('/home/rstudio/projects/Geneset_tracking.Rmd',params=list(species='Rat'), output_file='/home/rstudio/projects/Geneset_tracking_rat_$current_date.html')" > processing_output_Rat_$current_date.txt
 
 #copy the new file over to the web page
 cp "$(pwd)"/Geneset_tracking_rat_$current_date.html /home/geneset-cron/geneset_build_drive/EM_Genesets/current_release/Rat/README.html
